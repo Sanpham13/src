03397db09e173f8c75328dc8b6f07f5e6a2db5c9
Sanpham13 <s3915123@rmit.edu.vn>
2025-07-23 03:36:12 +0700

    trial

---
+++ rmitbot_description/launch/display.launch.py
@@ -0,0 +1,57 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command, LaunchConfiguration
+
+# This file is used to launch the robot in RViz2
+# It will load the 
+# - URDF file, for robot_state_publisher
+# - robot_state_publisher: publish TF tree, for rviz
+# - joint_state_publisher_gui: publish joint_state, for robot_state_publisher
+# - RViz2, needs TF from robot_state_publisher and joint_state from joint_state_publisher_gui
+
+def generate_launch_description():
+    # Get the directory of the package
+    rmitbot_description_dir = get_package_share_directory("rmitbot_description")
+    
+    # Declare the model argument
+    model_arg = DeclareLaunchArgument(
+        name="model", 
+        default_value=os.path.join(rmitbot_description_dir, 'urdf', 'rmitbot.urdf.xacro'),
+        description="Absolute path to robot urdf file"
+    )
+    
+    # This line processes your robot’s .xacro file at launch time, converting it to URDF
+    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]), value_type=str)
+    
+    # This node publishes the robot state to the TF tree
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{"robot_description": robot_description}]
+        )
+    
+    # This node publishes the joint state to the TF tree
+    joint_state_publisher = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+    )
+    
+    # This node launches RViz2 with the specified configuration file
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', os.path.join(rmitbot_description_dir, 'rviz', 'display.rviz')],
+    )
+    
+    return LaunchDescription([
+        model_arg, 
+        robot_state_publisher, 
+        joint_state_publisher,
+        rviz_node
+    ])
---
+++ rmitbot_description/launch/gazebo.launch.py
@@ -0,0 +1,77 @@
+import os
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+# This file is used to launch the robot in Gazebo
+# It will load the 
+# - URDF file, 
+# - robot_state_publisher, 
+# - bunch of package from gazebo-gz
+
+def generate_launch_description():
+    # Get the directory of the package
+    rmitbot_description_dir = get_package_share_directory("rmitbot_description")
+    
+    # Declare the model argument
+    model_arg = DeclareLaunchArgument(
+        name="model", 
+        default_value=os.path.join(rmitbot_description_dir, "urdf", "rmitbot.urdf.xacro"),
+        description="Absolute path to robot urdf file"
+    )
+
+    # This line processes your robot’s .xacro file at launch time, converting it to URDF
+    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]), value_type=str)
+
+    # This node publishes the robot state to the TF tree
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": True}]
+    )
+
+    # This node sets the environment variable for Gazebo resource path
+    # This line makes sure Gazebo can find your robot model
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[str(Path(rmitbot_description_dir).parent.resolve())]
+    )
+    
+    # This node launches Gazebo with the specified configuration file
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [os.path.join(get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+                launch_arguments=[("gz_args", [" -v 4", " -r", " empty.sdf", " --render-engine", " ogre"])]
+    )
+
+    # This node spawns the robot in Gazebo
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-topic", "robot_description","-name", "rmitbot"],
+    )
+
+    # This node bridges the clock messages between ROS2 and Gazebo
+    gz_ros2_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=["/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock"], 
+    )
+
+    return LaunchDescription([
+        model_arg,
+        gazebo_resource_path,
+        robot_state_publisher_node,
+        gazebo,
+        gz_spawn_entity,
+        gz_ros2_bridge,
+    ])
